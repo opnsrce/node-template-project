4a4787cb8ab3ed47915f924c38d6fc0a5603dbd7
Levi Hackwith <levi.hackwith@gmail.com>
2023-11-09 23:26:59 +0000

    Use Prettier for linting

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,8 +1,8 @@
-import {describe, expect, it} from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
 import add from "./index";
 
 describe("add", () => {
-  it("should add two numbers together", () => {
-    expect(add(4, 3)).toBe(7);
-  });
+    it("should add two numbers together", () => {
+        expect(add(4, 3)).toBe(7);
+    });
 });
